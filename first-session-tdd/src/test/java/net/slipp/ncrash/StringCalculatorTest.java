3db8873a63b5a643c5dbb2f219633666359bc9f9
Daekwon Kang <daekwon.kang@gmail.com>
2013-05-03 09:00:21 +0900

    빈 문자열을 입력할 경우 0을 반환해야 한다.

---
+++ first-session-tdd/src/test/java/net/slipp/ncrash/StringCalculatorTest.java
@@ -0,0 +1,18 @@
+package net.slipp.ncrash;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void 빈_문자열을_입력할_경우_0을_반환해야_한다() throws Exception {
+		StringCalculator calculator = new StringCalculator();
+		
+		assertThat(calculator.calc(""), equalTo(0));
+	}
+
+}
