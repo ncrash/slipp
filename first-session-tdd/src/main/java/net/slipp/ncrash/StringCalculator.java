efbd08a4c36ea8393f722c08642b3dd0b5b9d424
Daekwon Kang <daekwon.kang@gmail.com>
2013-05-06 08:07:50 +0900

    숫자_두개를_컴마_구분자로_입력할_경우_두_숫자의_합을_반환한다 - 테스트 동작 : 초록불

--- first-session-tdd/src/main/java/net/slipp/ncrash/StringCalculator.java
+++ first-session-tdd/src/main/java/net/slipp/ncrash/StringCalculator.java
@@ -2,13 +2,24 @@ package net.slipp.ncrash;
 
 public class StringCalculator {
 
+	private int number;
+
 	public int add(String text) {
 		// TODO Auto-generated method stub
 		if (text.isEmpty()) {
 			return 0;
-		}
+		} 
 		
-		int number = Integer.parseInt(text);
+		if (text.contains(",")) {
+			String[] tokens = text.split(",");
+			number = 0;
+			
+			for (int i = 0; i < tokens.length; i++) {
+				number += Integer.parseInt(tokens[i]);
+			}
+		} else {
+			number = Integer.parseInt(text);
+		}
 		
 		return number;
 	}
