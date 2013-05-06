f20fcf31a2359ac2269168fc95ba2e7a8fc4108a
Daekwon Kang <daekwon.kang@gmail.com>
2013-05-06 22:00:37 +0900

    더블_슬레쉬를_사용해_커스텀_구분자를_지정할_수_있다 : 테스트 동작 - 초록색

--- first-session-tdd/src/main/java/net/slipp/ncrash/StringCalculator.java
+++ first-session-tdd/src/main/java/net/slipp/ncrash/StringCalculator.java
@@ -1,5 +1,8 @@
 package net.slipp.ncrash;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
 	private int number;
@@ -8,19 +11,29 @@ public class StringCalculator {
 		// TODO Auto-generated method stub
 		if (text.isEmpty()) {
 			return 0;
-		} 
-		
-		if (text.contains(",")) {
-			String[] tokens = text.split(",|\n");
+		}
+
+		if (text.startsWith("//")) {
+			Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+			m.find();
+			String customDelimeter = m.group(1);
+			String[] customDelimeterResult = m.group(2).split(customDelimeter);
 			number = 0;
 			
+			for (int i = 0; i < customDelimeterResult.length; i++) {
+					number += Integer.parseInt(customDelimeterResult[i]);
+			}
+		} else if (text.contains(",")) {
+			String[] tokens = text.split(",|\n");
+			number = 0;
+
 			for (int i = 0; i < tokens.length; i++) {
 				number += Integer.parseInt(tokens[i]);
 			}
 		} else {
 			number = Integer.parseInt(text);
 		}
-		
+
 		return number;
 	}
 
