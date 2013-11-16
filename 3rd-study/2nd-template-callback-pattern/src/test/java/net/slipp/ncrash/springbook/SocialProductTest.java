deac903273a570a1ba2ee9b6886ef3553cb1cae8
Daekwon Kang <daekwon.kang@gmail.com>
2013-11-16 23:28:28 +0900

    테스트 케이스 비교 내용 보완

--- 3rd-study/2nd-template-callback-pattern/src/test/java/net/slipp/ncrash/springbook/SocialProductTest.java
+++ 3rd-study/2nd-template-callback-pattern/src/test/java/net/slipp/ncrash/springbook/SocialProductTest.java
@@ -30,6 +30,11 @@ public class SocialProductTest {
 		
 		logger.debug("coupang today product : " + todayProductList.toString());
 		assertThat(todayProductList.size(), is(5));
+		assertThat(todayProductList.get(0), is("/deal.pang?coupang=48884870&areacode=MB&order=1"));
+		assertThat(todayProductList.get(1), is("/deal.pang?coupang=48884870&areacode=MB&order=1"));
+		assertThat(todayProductList.get(2), is("/deal.pang?coupang=49080218&areacode=MB&order=2"));
+		assertThat(todayProductList.get(3), is("/deal.pang?coupang=49080218&areacode=MB&order=2"));
+		assertThat(todayProductList.get(4), is("/deal.pang?coupang=48822408&areacode=MB&order=3"));
 	}
 	
 	@Test
@@ -41,6 +46,11 @@ public class SocialProductTest {
 		
 		logger.debug("wemakeprice today product : " + todayProductList.toString());
 		assertThat(todayProductList.size(), is(5));
+		assertThat(todayProductList.get(0), is("http://pubads.g.doubleclick.net/gampad/jump?iu=/21948295/big_01_1326x340&sz=1326x340&c=1486657768&t=gender%3Dunknown%26age%3Dunknown"));
+		assertThat(todayProductList.get(1), is("http://pubads.g.doubleclick.net/gampad/jump?iu=/21948295/big_02_1326x340&sz=1326x340&c=1317307108&t=gender%3Dunknown%26age%3Dunknown"));
+		assertThat(todayProductList.get(2), is("http://pubads.g.doubleclick.net/gampad/jump?iu=/21948295/big_03_1326x340&sz=1326x340&c=1467005572&t=gender%3Dunknown%26age%3Dunknown"));
+		assertThat(todayProductList.get(3), is("http://pubads.g.doubleclick.net/gampad/jump?iu=/21948295/big_04_1326x340&sz=1326x340&c=1657990271&t=gender%3Dunknown%26age%3Dunknown"));
+		assertThat(todayProductList.get(4), is("http://pubads.g.doubleclick.net/gampad/jump?iu=/21948295/big_05_1326x340&sz=1326x340&c=12740207&t=gender%3Dunknown%26age%3Dunknown"));
 	}
 	
 	@Test
@@ -52,5 +62,10 @@ public class SocialProductTest {
 		
 		logger.debug("tmon today product : " + todayProductList.toString());
 		assertThat(todayProductList.size(), is(5));
+		assertThat(todayProductList.get(0), is("/deal/38642685/101001?utm=TODAYSHOT&loc=25"));
+		assertThat(todayProductList.get(1), is("/deal/38841901/101001?utm=TODAYSHOT&loc=26"));
+		assertThat(todayProductList.get(2), is("/deal/39614561/101001?utm=TODAYSHOT&loc=27"));
+		assertThat(todayProductList.get(3), is("/deal/38483317/101001?utm=TODAYSHOT&loc=28"));
+		assertThat(todayProductList.get(4), is("/deal/39611129/101001?utm=TODAYSHOT&loc=29"));
 	}
 }
