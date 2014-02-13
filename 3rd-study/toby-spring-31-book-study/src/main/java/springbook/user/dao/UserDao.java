1bb4d697d5af7f60eb9e5c75ba4b5ff81c74430a
Daekwon Kang <daekwon.kang@gmail.com>
2014-02-13 10:14:17 +0900

    Ch3 - 3.6.1 스프링의 JdbcTemplate
    리스트 3-45 JdbcTemplate의 초기화를 위한 코드
    리스트 3-47 내장 콜백을 사용하는 update()로 변경한 deleteAll() 메소드
    리스트 3-48 내장 콜백을 사용하는 update()로 변경한 add() 메소드

--- 3rd-study/toby-spring-31-book-study/src/main/java/springbook/user/dao/UserDao.java
+++ 3rd-study/toby-spring-31-book-study/src/main/java/springbook/user/dao/UserDao.java
@@ -5,6 +5,7 @@ package springbook.user.dao;
  */
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
@@ -15,30 +16,17 @@ import java.sql.SQLException;
 
 public class UserDao {
     private DataSource dataSource;
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
-        this.jdbcContext = new JdbcContext();
-        this.jdbcContext.setDataSource(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
 
         this.dataSource = dataSource;
     }
 
     public void add(final User user) throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(
-                new StatementStrategy() {
-                    public PreparedStatement makePreparedStatement(Connection c)
-                            throws SQLException {
-                        PreparedStatement ps =
-                                c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-                        ps.setString(1, user.getId());
-                        ps.setString(2, user.getName());
-                        ps.setString(3, user.getPassword());
-
-                        return ps;
-                    }
-                }
-        );
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
 
@@ -68,7 +56,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from users");
+        this.jdbcTemplate.update("delete from users");
     }
 
     public int getCount() throws SQLException  {
