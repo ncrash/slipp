01354fd0d5d5ee902ac59adc231b0fe7cfd53b79
Daekwon Kang <daekwon.kang@gmail.com>
2014-02-17 13:53:21 +0900

    Ch5 - 서비스 추상화 - 5.1.1
    5.1 사용자 레벨 관리 기능 추가
    
    리스트 5-5 수정된 테스트 픽스쳐
    리스트 5-7 새로운 필드를 포함하는 User 필드 값 검증 메소드
    리스트 5-9 추가된 필드를 위한 UserDaoJdbc의 수정코드
    리스트 5-10 사용자 정보 수정 메소드 테스트
    리스트 5-12 사용자 정보 수정용 update() 메소드

--- 3rd-study/toby-spring-31-book-study/src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ 3rd-study/toby-spring-31-book-study/src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -6,6 +6,7 @@ package springbook.user.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
@@ -14,11 +15,11 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoJdbc implements UserDao {
-    private JdbcTemplate jdbcTemplate;
-
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
+    private JdbcTemplate jdbcTemplate;
 
     private RowMapper<User> userMapper =
             new RowMapper<User>() {
@@ -27,14 +28,19 @@ public class UserDaoJdbc implements UserDao {
                     user.setId(rs.getString("id"));
                     user.setName(rs.getString("name"));
                     user.setPassword(rs.getString("password"));
+                    user.setLevel(Level.valueOf(rs.getInt("level")));
+                    user.setLogin(rs.getInt("login"));
+                    user.setRecommend(rs.getInt("recommend"));
                     return user;
                 }
             };
 
-
-    public void add(final User user) {
-        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
-                user.getId(), user.getName(), user.getPassword());
+    public void add(User user) {
+        this.jdbcTemplate.update(
+                "insert into users(id, name, password, level, login, recommend) " +
+                        "values(?,?,?,?,?,?)",
+                user.getId(), user.getName(), user.getPassword(),
+                user.getLevel().intValue(), user.getLogin(), user.getRecommend());
     }
 
     public User get(String id) {
--- 3rd-study/toby-spring-31-book-study/src/main/java/springbook/user/dao/UserDaoTest.java
+++ 3rd-study/toby-spring-31-book-study/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -22,16 +22,14 @@ import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/test-applicationContext.xml")
 public class UserDaoTest {
-    @Autowired
-    UserDao dao;
-
-    @Autowired
-    DataSource dataSource;
+    @Autowired UserDao dao;
+    @Autowired DataSource dataSource;
 
     private User user1;
     private User user2;
@@ -39,9 +37,9 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        this.user1 = new User("gyumee", "박성철", "springno1");
-        this.user2 = new User("leegw700", "이길원", "springno2");
-        this.user3 = new User("bumjin", "박범진", "springno3");
+        this.user1 = new User("gyumee", "박성철", "springno1", Level.BASIC, 1, 0);
+        this.user2 = new User("leegw700", "이길원", "springno2", Level.SILVER, 55, 10);
+        this.user3 = new User("bumjin", "박범진", "springno3", Level.GOLD, 100, 40);
     }
 
     @Test
@@ -54,12 +52,10 @@ public class UserDaoTest {
         assertThat(dao.getCount(), is(2));
 
         User userget1 = dao.get(user1.getId());
-        assertThat(userget1.getName(), is(user1.getName()));
-        assertThat(userget1.getPassword(), is(user1.getPassword()));
+        checkSameUser(userget1, user1);
 
         User userget2 = dao.get(user2.getId());
-        assertThat(userget2.getName(), is(user2.getName()));
-        assertThat(userget2.getPassword(), is(user2.getPassword()));
+        checkSameUser(userget2, user2);
     }
 
     @Test(expected=EmptyResultDataAccessException.class)
@@ -116,6 +112,9 @@ public class UserDaoTest {
         assertThat(user1.getId(), is(user2.getId()));
         assertThat(user1.getName(), is(user2.getName()));
         assertThat(user1.getPassword(), is(user2.getPassword()));
+        assertThat(user1.getLevel(), is(user2.getLevel()));
+        assertThat(user1.getLogin(), is(user2.getLogin()));
+        assertThat(user1.getRecommend(), is(user2.getRecommend()));
     }
 
     @Test(expected=DuplicateKeyException.class)
