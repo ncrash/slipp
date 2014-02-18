c996ae0a31299849d361a235fe06596c14f36470
Daekwon Kang <daekwon.kang@gmail.com>
2014-02-18 11:25:31 +0900

    Ch6 - AOP
    6.5.1 자동 프록시 생성
    6.5.2 DefaultAdvisorAutoProxyCreator의 적용

--- 3rd-study/toby-spring-31-book-study/src/test/java/springbook/user/service/UserServiceTest.java
+++ 3rd-study/toby-spring-31-book-study/src/test/java/springbook/user/service/UserServiceTest.java
@@ -19,13 +19,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -38,11 +36,12 @@ import springbook.user.domain.User;
 @ContextConfiguration(locations="/test-applicationContext.xml")
 public class UserServiceTest {
     @Autowired UserService userService;
+    @Autowired UserService testUserService;
     @Autowired UserDao userDao;
-    @Autowired UserServiceImpl userServiceImpl;
     @Autowired MailSender mailSender;
     @Autowired PlatformTransactionManager transactionManager;
     @Autowired ApplicationContext context;
+
 
     List<User> users;	// test fixture
 
@@ -182,22 +181,12 @@ public class UserServiceTest {
     }
 
     @Test
-    @DirtiesContext
     public void upgradeAllOrNothing() {
-        TestUserService testUserService = new TestUserService(users.get(3).getId());
-        testUserService.setUserDao(userDao);
-        testUserService.setMailSender(mailSender);
-
-        ProxyFactoryBean txProxyFactoryBean =
-                context.getBean("&userService", ProxyFactoryBean.class);
-        txProxyFactoryBean.setTarget(testUserService);
-        UserService txUserService = (UserService) txProxyFactoryBean.getObject();
-
         userDao.deleteAll();
         for(User user : users) userDao.add(user);
 
         try {
-            txUserService.upgradeLevels();
+            testUserService.upgradeLevels();
             fail("TestUserServiceException expected");
         }
         catch(TestUserServiceException e) {
@@ -206,12 +195,8 @@ public class UserServiceTest {
         checkLevelUpgraded(users.get(1), false);
     }
 
-    public static class TestUserService extends UserServiceImpl {
-        private String id;
-
-        public TestUserService(String id) {
-            this.id = id;
-        }
+    static class TestUserServiceImpl extends UserServiceImpl {
+        private String id = "madnite1"; // users(3).getId()
 
         protected void upgradeLevel(User user) {
             if (user.getId().equals(this.id)) throw new TestUserServiceException();
