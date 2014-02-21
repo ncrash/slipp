a8f26cd7cc85899ac4657fac9c6138a9a1ee7dd8
Daekwon Kang <daekwon.kang@gmail.com>
2014-02-21 17:19:26 +0900

    Ch7 - 스프링 핵심 기술의 응용 - 7.3.1
    7.2 인터페이스의 분리와 자기참조 빈
    
    7.2.6 디폴트 의존관계

--- 3rd-study/toby-spring-31-book-study/src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
+++ 3rd-study/toby-spring-31-book-study/src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
@@ -11,20 +11,21 @@ import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
 public class JaxbXmlSqlReader implements SqlReader {
-	private String sqlmapFile;
+    private final String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
+    private String sqlmapFile = DEFAULT_SQLMAP_FILE;
 
-	public void setSqlmapFile(String sqlmapFile) { this.sqlmapFile = sqlmapFile; }
+    public void setSqlmapFile(String sqlmapFile) { this.sqlmapFile = sqlmapFile; }
 
-	public void read(SqlRegistry sqlRegistry) {
-		String contextPath = Sqlmap.class.getPackage().getName(); 
-		try {
-			JAXBContext context = JAXBContext.newInstance(contextPath);
-			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream is = UserDao.class.getResourceAsStream(sqlmapFile);
-			Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
-			for(SqlType sql : sqlmap.getSql()) {
-				sqlRegistry.registerSql(sql.getKey(), sql.getValue());
-			}
-		} catch (JAXBException e) { throw new RuntimeException(e); } 		
-	}
+    public void read(SqlRegistry sqlRegistry) {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = UserDao.class.getResourceAsStream(sqlmapFile);
+            Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
+            for(SqlType sql : sqlmap.getSql()) {
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) { throw new RuntimeException(e); }
+    }
 }
